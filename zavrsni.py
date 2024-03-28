2427ab609b886f95666871c03eee9dcfb8489f2e
Mateo SIndija <mtsindija05@gmail.com>
2024-03-28 09:37:19 +0100

    Fixed some things

--- zavrsni.py
+++ zavrsni.py
@@ -13,10 +13,29 @@ import pmdarima as pm
 from datetime import timedelta
 
 def calculate_average_weekly_sales():
-    selected_store_data['Week_of_Year'] = selected_store_data['Date'].dt.isocalendar().week
-    average_weekly_sales = selected_store_data.groupby('Week_of_Year')['Weekly_Sales'].mean()
-    print(f'Average sales by weeks for store {store_number} {average_weekly_sales} ')
+    merged_data['Date'] = pd.to_datetime(merged_data['Date'])
     
+    # Extract week number from the 'Date' column
+    merged_data['Week_Number'] = merged_data['Date'].dt.isocalendar().week
+    
+    # Compute the total weekly sales for each week
+    weekly_sales_total = merged_data.groupby('Week_Number')['Weekly_Sales'].sum()
+    
+    # Compute the total number of departments for each week
+    department_count = merged_data.groupby('Week_Number')['Dept'].nunique()
+    
+    # Calculate the average weekly sales per department
+    average_weekly_sales = weekly_sales_total / department_count
+    
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    average_weekly_sales.plot(kind='bar', color='skyblue')
+    plt.title('Average Weekly Sales by Week')
+    plt.xlabel('Week Number')
+    plt.ylabel('Average Weekly Sales')
+    plt.xticks(rotation=45)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
 
 def show_fourier_transform(selected_store):
     store_data = merged_data[merged_data['Store'] == selected_store].copy()
@@ -34,9 +53,6 @@ def show_fourier_transform(selected_store):
     # Plot the Fourier transformation
     plt.figure(figsize=(10, 6))
     plt.plot(freq, np.abs(fourier_transform))
-    plt.scatter(dominant_frequency, dominant_amplitude, color='red', label='Dominant Frequency')
-    plt.text(dominant_frequency, dominant_amplitude, f'({dominant_frequency:.2f}, {dominant_amplitude:.2f})',
-             verticalalignment='bottom', horizontalalignment='left', color='red', fontsize=10)
     plt.title(f'Fourier Transformation of Weekly Sales for Store {selected_store}')
     plt.xlabel('Frequency')
     plt.ylabel('Amplitude')
@@ -46,25 +62,21 @@ def show_fourier_transform(selected_store):
     plt.savefig(f'fourier_transform_{store_number}.png')
         
         
-def show_weekly_sales_by_year():
-    weekly_sales_by_year = selected_store_data.groupby(['Year', selected_store_data['Date'].dt.to_period("W")])['Weekly_Sales'].sum().reset_index()
-
-
-    plt.figure(figsize=(14, 6))
-    sns.set_palette("viridis", len(weekly_sales_by_year['Year'].unique())) 
+def show_weekly_sales_by_year(selected_store):
+    selected_store_data = merged_data[merged_data['Store'] == selected_store]
     
-    for year, data in weekly_sales_by_year.groupby('Year'):
-        sns.lineplot(x=data['Date'].dt.weekofyear, y=data['Weekly_Sales'], label=f'Year {year}', marker='o')  # Use week of the year as x-axis
-
-    plt.title(f'Weekly Sales for Store {store_number} by Year')
-    plt.xlabel('Week')
+    # Group by date and sum the weekly sales across all departments
+    weekly_sales_by_date = selected_store_data.groupby('Date')['Weekly_Sales'].sum()
+    
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(weekly_sales_by_date.index, weekly_sales_by_date.values, linestyle='-')
+    plt.title(f'Weekly Sales for Store {selected_store}')
+    plt.xlabel('Date')
     plt.ylabel('Weekly Sales')
-    plt.xticks(range(1, 53))  
-    plt.legend(title='Year', bbox_to_anchor=(1.05, 1), loc='upper left') 
-
     plt.grid(True)
+    plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.savefig(f'year_weekly_sales_{store_number}.png')
     plt.show()
     
 def svd_decomposition():
@@ -105,84 +117,108 @@ def growth_rate():
 
     print(f"The department with the highest average growth rate is Department {most_growth_dept}.")
     
-def correlation_between_stores(store_number):
+def correlation_between_stores():
 
-    store_df = merged_data.copy()
+    store_sales_data = merged_data[['Store', 'Weekly_Sales', 'Date']].copy()
     
-    correlation_matrix = store_df.pivot_table(index='Date', columns='Store', values='Weekly_Sales', aggfunc='mean').corr()
+    pivot_data = store_sales_data.pivot_table(values='Weekly_Sales', index='Date', columns='Store')
     
-    correlation_with_store = correlation_matrix[store_number]
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x=correlation_with_store.index, y=correlation_with_store.values, color='skyblue')
-    plt.title(f'Correlation of Weekly Sales with Store {store_number}')
-    plt.xlabel('Other Stores')
-    plt.ylabel('Correlation')
-    plt.savefig(f'correlation_between_store_{store_number}_and_other_stores.png')
+    non_constant_columns = pivot_data.columns[pivot_data.nunique() > 1]
+    pivot_data = pivot_data[non_constant_columns]
+    
+    # Check for NaN values and drop them
+    pivot_data = pivot_data.dropna()
+    
+    # Calculate the correlation matrix between stores
+    correlation_matrix = pivot_data.corr()
+    
+    # Plot the correlation matrix using Seaborn heatmap
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(correlation_matrix, cmap='coolwarm', linewidths=.5)
+    plt.title('Correlation Heatmap between Stores based on Weekly Sales')
+    plt.savefig(f'correlation_heatmap.png')   
     plt.show()
-
+    
+    
 def correlation_matrix():
-    selected_columns = ['Weekly_Sales', 'Fuel_Price', 'CPI', 'IsHoliday_y', 'Unemployment', 'Temperature']
+    summed_weekly_sales = merged_data.groupby('Store')['Weekly_Sales'].sum()
+    merged_store_data = merged_data.groupby('Store').first()
+    merged_store_data['Weekly_Sales'] = summed_weekly_sales
+    
 
-    selected_data = selected_store_data[selected_columns]
-
-    correlation_matrix = selected_data.corr()
+    correlation_matrix = merged_store_data[['Weekly_Sales', 'Size', 'Temperature', 'Fuel_Price', 'CPI', 'Unemployment']].corr()
+    
 
     plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=.5)
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
     plt.title('Correlation Matrix')
     plt.show()
-    print("Conclusions:")
-    print("-" * 40)
-    correlation_sales_holiday = correlation_matrix.loc['Weekly_Sales', 'IsHoliday_y']
-    correlation_sales_temperature = correlation_matrix.loc['Weekly_Sales', 'Temperature']
+    
 
-    for column in correlation_matrix.columns:
-        for index in correlation_matrix.index:
-            if column != index:
-                correlation_value = correlation_matrix.loc[index, column]
-                print(f"Correlation between {index} and {column}: {correlation_value:.2f}")
+
+    store_markdowns = merged_data.groupby('Store')[['MarkDown1', 'MarkDown2', 'MarkDown3', 'MarkDown4', 'MarkDown5']].sum()
+    store_data = pd.concat([summed_weekly_sales, store_markdowns], axis=1)
+    
+
+    correlation_matrix = store_data.corr()
         
-                if correlation_value > 0:
-                    print("Conclusion: There is a positive correlation.")
-                elif correlation_value < 0:
-                    print("Conclusion: There is a negative correlation.")
-                else:
-                    print("Conclusion: There is no significant linear relationship.")
-                
-                print("-" * 50)
-
-
-    total_sales_by_store = merged_data.groupby('Store')['Weekly_Sales'].sum()
-    store_sizes = merged_data.groupby('Store')['Size'].max()
-
-    store_sales_size_df = pd.DataFrame({'Total_Sales': total_sales_by_store, 'Store_Size': store_sizes})
-    correlation_sales_size = store_sales_size_df['Total_Sales'].corr(store_sales_size_df['Store_Size'])
-
-    print(f"Correlation between Total Sales and Store Size: {correlation_sales_size:.2f}")
-    print("-" * 50)
+    correlation_values = correlation_matrix.loc['Weekly_Sales', 'MarkDown1':'MarkDown5'].to_frame().T
     
-    markdown_columns = ["Weekly_Sales","MarkDown1", "MarkDown2", "MarkDown3", "MarkDown4", "MarkDown5"]
-    selected_data_markdown = selected_store_data[markdown_columns].copy()
+    # Plotting
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(correlation_values, cmap='coolwarm', annot=True, fmt=".2f", linewidths=.5)
+    plt.title('Correlation between Weekly Sales and Markdowns')
+    plt.xlabel('Markdowns')
+    plt.ylabel('Weekly Sales')
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=0)
+    plt.show()
 
-    selected_data_markdown.replace('NA', pd.NA, inplace=True)
-  
-    selected_data_markdown.fillna(0, inplace=True)
+        
+def most_profitable_quarter():
+
+    # Group the data by quarter and sum the weekly sales
+    merged_data['Quarter'] = merged_data['Date'].dt.to_period('Q')
+    quarterly_sales = merged_data.groupby('Quarter')['Weekly_Sales'].sum()
     
-
-    correlation_sales_markdown1 = selected_data_markdown['Weekly_Sales'].corr(selected_data_markdown['MarkDown1'])
-    correlation_sales_markdown2 = selected_data_markdown['Weekly_Sales'].corr(selected_data_markdown['MarkDown2'])
-    correlation_sales_markdown3 = selected_data_markdown['Weekly_Sales'].corr(selected_data_markdown['MarkDown3'])
-    correlation_sales_markdown4 = selected_data_markdown['Weekly_Sales'].corr(selected_data_markdown['MarkDown4'])
-    correlation_sales_markdown5 = selected_data_markdown['Weekly_Sales'].corr(selected_data_markdown['MarkDown5'])
+    # Find the quarter with the highest total sales
+    most_profitable_quarter = quarterly_sales.idxmax()
+    total_sales_most_profitable_quarter = quarterly_sales.max()
     
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    quarterly_sales.plot(kind='bar', color='skyblue')
+    plt.title('Quarterly Sales')
+    plt.xlabel('Quarter')
+    plt.ylabel('Total Sales')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
 
-    print(f"Correlation with MarkDown1: {correlation_sales_markdown1:.2f}")
-    print(f"Correlation with MarkDown2: {correlation_sales_markdown2:.2f}")
-    print(f"Correlation with MarkDown3: {correlation_sales_markdown3:.2f}")
-    print(f"Correlation with MarkDown4: {correlation_sales_markdown4:.2f}")
-    print(f"Correlation with MarkDown5: {correlation_sales_markdown5:.2f}")
-    print("-" * 50)
-
+def most_profitable_weeks():
+    merged_data['Date'] = pd.to_datetime(merged_data['Date'])
+    
+    # Extract week number from the 'Date' column using isocalendar method
+    merged_data['Week_Number'] = merged_data['Date'].dt.isocalendar().week
+    
+    # Group data by week number and calculate total profit (sum of weekly sales) for each week
+    weekly_profit = merged_data.groupby('Week_Number')['Weekly_Sales'].sum().reset_index()
+    
+    # Sort weeks by total profit in descending order to find the weeks with highest profit
+    most_profitable_weeks = weekly_profit.sort_values(by='Weekly_Sales', ascending=False)
+    
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.bar(weekly_profit['Week_Number'], weekly_profit['Weekly_Sales'], color='blue', label='Total Weekly Sales')
+    plt.bar(most_profitable_weeks['Week_Number'][:5], most_profitable_weeks['Weekly_Sales'][:5], color='red', label='Most Profitable Weeks')
+    plt.xlabel('Week Number')
+    plt.ylabel('Total Weekly Sales')
+    plt.title('Total Weekly Sales')
+    plt.legend()
+    plt.grid(axis='y')
+    plt.xticks(range(1, 53))
+    plt.show()
+    
 def sales_forecast(selected_store):
 # Select a store for SARIMA modeling
   
@@ -193,7 +229,7 @@ def sales_forecast(selected_store):
     # Prepare data for SARIMA model
     date_sales = store_data[['Date', 'Weekly_Sales']].set_index('Date')
     date_sales.index = pd.to_datetime(date_sales.index)
-    date_sales = date_sales.resample('W').sum()  # Resample to weekly frequency
+    date_sales = date_sales.resample('W').sum()  
     
     # Train-test split
     train_size = int(len(date_sales) * 0.8)
@@ -231,63 +267,106 @@ def sales_forecast(selected_store):
     plt.legend()
     plt.show()
     plt.savefig(f'sarima_model_{store_number}.png')
+    
 
-def seasonal_decomposition():
-    selected_store_data_copy = selected_store_data.copy()
+def seasonal_decomposition(store_number):
+    selected_store_data = merged_data[merged_data['Store'] == store_number]
     
-    time_series = selected_store_data_copy['Weekly_Sales']
-
-    # Try different period values and observe the results
-    periods_to_try = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 100]
+    # Aggregate weekly sales across departments for the selected store
+    weekly_sales = selected_store_data.groupby('Date')['Weekly_Sales'].sum()
     
-    best_period = None
-    best_quality_measure = float('inf')  # Initialize with a high value
+    # Perform seasonal decomposition
+    decomposition = seasonal_decompose(weekly_sales, model='multiplicative', period=52)  # Assuming weekly data
     
-    for period in periods_to_try:
-        # Perform seasonal decomposition
-        result = seasonal_decompose(time_series, model='additive', period=period)
-        
-        # Calculate a quality measure (e.g., standard deviation of the seasonal component)
-        quality_measure = np.std(result.seasonal)
-        
-        # Update the best period if the current one is better
-        if quality_measure < best_quality_measure:
-            best_quality_measure = quality_measure
-            best_period = period
-    
-    # Print the best period and quality measure
-    print(f'Best Period: {best_period}')
-    print(f'Corresponding Quality Measure (e.g., std of seasonal component): {best_quality_measure}')
-    
-    # Plot the components for the best period
-    result = seasonal_decompose(time_series, model='additive', period=best_period)
+    # Plot the decomposition
     plt.figure(figsize=(12, 8))
     
-    # Original time series
-    plt.subplot(4, 1, 1)
-    plt.plot(time_series)
-    plt.title('Original Time Series')
+    plt.subplot(411)
+    plt.plot(weekly_sales, label='Original')
+    plt.legend()
     
-    # Trend component
-    plt.subplot(4, 1, 2)
-    plt.plot(result.trend)
-    plt.title('Trend Component')
+    plt.subplot(412)
+    plt.plot(decomposition.trend, label='Trend')
+    plt.legend()
     
-    # Seasonal component
-    plt.subplot(4, 1, 3)
-    plt.plot(result.seasonal)
-    plt.title('Seasonal Component')
+    plt.subplot(413)
+    plt.plot(decomposition.seasonal, label='Seasonal')
+    plt.legend()
     
-    # Residual component
-    plt.subplot(4, 1, 4)
-    plt.plot(result.resid)
-    plt.title('Residual Component')
+    plt.subplot(414)
+    plt.plot(decomposition.resid, label='Residual')
+    plt.legend()
+    
     
     plt.tight_layout()
-    plt.savefig(f'seasonal_decomposition_{store_number}.png')
+    plt.show()
+
+def store_sales_graphs():
+    max_sales_store = merged_data.groupby('Store')['Weekly_Sales'].sum().idxmax()
+    
+    # Find the store with minimum sales
+    min_sales_store = merged_data.groupby('Store')['Weekly_Sales'].sum().idxmin()
+    
+    # Find the store with median sales
+    median_sales_store = merged_data.groupby('Store')['Weekly_Sales'].sum().median()
+    
+    # Find the store closest to the median sales
+    median_sales_store = merged_data.groupby('Store')['Weekly_Sales'].sum().sub(median_sales_store).abs().idxmin()
+    
+    
+        
+    # Filter data for the selected stores
+    selected_stores = merged_data[merged_data['Store'].isin([max_sales_store, min_sales_store, median_sales_store])]
+    
+    total_sales_by_store = selected_stores.groupby(['Store', 'Date'])['Weekly_Sales'].sum().reset_index()
+
+    # Plot the total weekly sales for each selected store
+    plt.figure(figsize=(12, 8))
+    for store, data in total_sales_by_store.groupby('Store'):
+        plt.plot(data['Date'], data['Weekly_Sales'], label=f'Store {store}')
+    
+    plt.title('Total Weekly Sales for stores with max, min and median sales')
+    plt.xlabel('Date')
+    plt.ylabel('Total Weekly Sales')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+    store_sales = merged_data.groupby('Store')['Weekly_Sales'].sum()
+    store_sizes = merged_data.groupby('Store')['Size'].first()
+    
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.scatter(store_sizes, store_sales, alpha=0.5)
+    plt.title('Effect of Store Size on Total Weekly Sales')
+    plt.xlabel('Store Size')
+    plt.ylabel('Total Weekly Sales')
+    plt.grid(True)
+    plt.show()
+      
+def impactful_departments():
+    
+    store_dept_sales = merged_data.groupby(['Store', 'Dept'])['Weekly_Sales'].sum().reset_index()
+    
+    # Finding the department with the highest sales for each store
+    most_impactful_dept = store_dept_sales.loc[store_dept_sales.groupby('Store')['Weekly_Sales'].idxmax()]
+    
+    # Plotting the most impactful departments for each store
+    plt.figure(figsize=(10, 6))
+    plt.bar(most_impactful_dept['Store'], most_impactful_dept['Weekly_Sales'])
+    plt.title('Most Impactful Departments for Each Store')
+    plt.xlabel('Store')
+    plt.ylabel('Total Weekly Sales')
+    plt.xticks(most_impactful_dept['Store'])
+    for i, dept in enumerate(most_impactful_dept['Dept']):
+        plt.text(i + 1, most_impactful_dept['Weekly_Sales'].iloc[i], str(dept), ha='center', va='bottom')
+    plt.grid(axis='y')
     plt.show()
 
 
+plt.tight_layout()
+plt.show()
 sales_data = pd.read_csv('sales_data_set.csv')
 stores_data = pd.read_csv('stores_data_set.csv')
 features_data = pd.read_csv('features_data_set.csv')
@@ -305,7 +384,7 @@ merged_data['Unemployment'].interpolate(method='linear', inplace=True)
 merged_data['CPI'].interpolate(method='linear', inplace=True)
 merged_data = merged_data.drop(columns='IsHoliday_x', errors='ignore')
 
-store_number = 9  # Change this to the desired store number
+store_number = 42  # Change this to the desired store number
 max_store_number = 44  # Maximum allowed store number
 
 # Check if the store number is within the allowed range
@@ -319,16 +398,22 @@ if 1 <= store_number <= max_store_number:
    
     selected_store_data['Year'] = selected_store_data['Date'].dt.year
 
-    show_weekly_sales_by_year()
-    calculate_average_weekly_sales()
-    show_fourier_transform(store_number)
-    svd_decomposition()
-    growth_rate()
-    correlation_matrix()
-    total_sale()
-    seasonal_decomposition()
-    sales_forecast(store_number)
-    correlation_between_stores(store_number)
+    #show_weekly_sales_by_year(store_number)
+    #calculate_average_weekly_sales()
+    #show_fourier_transform(store_number)
+    #svd_decomposition()
+    #growth_rate()
+    #correlation_matrix()
+    #total_sale()
+    #seasonal_decomposition(store_number)
+    #sales_forecast(store_number)
+    #correlation_between_stores()
+    #most_profitable_quarter()
+    #most_profitable_weeks()
+    #impactful_departments()
+    #store_sales_graphs()
+
+    
     
     
 else:
